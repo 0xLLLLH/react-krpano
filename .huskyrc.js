b7ba9de3d7d5bd357cbfa91b28c0500d67929d19
0xLLLLH <linlihao159@gmail.com>
2021-03-03 23:15:48 +0800

    chore: 🗯 更新huskyrc

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     hooks: {
         'pre-commit': 'lint-staged',
-        'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
+        // 'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
         'commit-msg': 'commitlint -e $GIT_PARAMS',
     },
 };
